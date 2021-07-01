0c21058c0bf6f190f452d0f7c06478aaf4c1590f
Preet Singhwal <preetsinghwal38@gmail.com>
2021-07-01 12:47:56 +0530

    Add redux to ContactFrom and Checkout

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -11,12 +11,7 @@ import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 import * as actionTypes from '../../store/actions';
 
 
-const INGREDIENT_PRICES = {
-  salad: 0.5,
-  cheese: 0.4,
-  meat: 1.3,
-  bacon: 0.7,
-};
+
 
 class BurgerBuilder extends Component {
   /* OLD METHOD OF DEFINING STATE */
@@ -29,9 +24,6 @@ class BurgerBuilder extends Component {
 
   /* LATEST METHOD */
   state = {
-    
-    totalPrice: 4,
-    purchasable: false,
     purchasing: false,
     loading: false,
     error: false,
@@ -64,40 +56,10 @@ class BurgerBuilder extends Component {
         return sum + el;
       }, 0);
 
-    this.setState({ purchasable: sum > 0 });
+    return sum > 0 ;
   }
 
-  addIngredientHandler = (type) => {
-    const oldCount = this.state.ingredients[type];
-    const updateCount = oldCount + 1;
-    const updatedIngredients = {
-      ...this.state.ingredients,
-    };
-    updatedIngredients[type] = updateCount;
-    const priceAddition = INGREDIENT_PRICES[type];
-    const oldPrice = this.state.totalPrice;
-    const newPrice = oldPrice + priceAddition;
-    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
-    this.updatePurchaseState(updatedIngredients);
-  };
-
-  removeIngredientHandler = (type) => {
-    const oldCount = this.state.ingredients[type];
-    if (oldCount <= 0) {
-      return;
-    }
-    const updateCount = oldCount - 1;
-    const updatedIngredients = {
-      ...this.state.ingredients,
-    };
-    updatedIngredients[type] = updateCount;
-    const priceDeduction = INGREDIENT_PRICES[type];
-    const oldPrice = this.state.totalPrice;
-    const newPrice = oldPrice - priceDeduction;
-    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
-    this.updatePurchaseState(updatedIngredients);
-  };
-
+  
   purchaseHandler = () => {
     this.setState({ purchasing: true });
   };
@@ -107,26 +69,7 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    //alert('You Continue);
-
-    const queryParam = [];
-    for (let i in this.state.ingredients) {
-      // Here we are setting ket value pair
-      queryParam.push(
-        encodeURIComponent(i) +
-          "=" +
-          encodeURIComponent(this.state.ingredients[i])
-      );
-    }
-
-    queryParam.push("price=" + this.state.totalPrice);
-
-    const queryString = queryParam.join("&");
-
-    this.props.history.push({
-      pathname: "/checkout",
-      search: "?" + queryString,
-    });
+    this.props.history.push('/checkout');
   };
 
   render() {
@@ -153,8 +96,8 @@ class BurgerBuilder extends Component {
             ingredientAdded={this.props.onIngredientAdded}
             ingredientRemoved={this.props.onIngredientRemoved}
             disabled={disabledInfo}
-            price={this.state.totalPrice}
-            purchasable={this.state.purchasable}
+            price={this.props.price}
+            purchasable={this.updatePurchaseState(this.props.ings)}
             ordered={this.purchaseHandler}
           />
         </Auxillary>
@@ -165,7 +108,7 @@ class BurgerBuilder extends Component {
           ingredients={this.props.ings}
           purchaseCancelled={this.purchaseCancelHandler}
           purchaseContinued={this.purchaseContinueHandler}
-          price={this.state.totalPrice}
+          price={this.props.price}
         />
       );
     }
@@ -192,7 +135,8 @@ class BurgerBuilder extends Component {
 
 const mapStateToProps = state =>{
   return {
-    ings : state.ingredients
+    ings : state.ingredients,
+    price : state.totalPrice
   };
 }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -10,6 +10,13 @@ const initialState = {
     totalPrice  : 4
 };
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7,
+  };
+
 const reducer = (state = initialState, action) =>{
 
     switch (action.type) {
@@ -20,7 +27,8 @@ const reducer = (state = initialState, action) =>{
                 ingredients : {
                     ...state.ingredients,
                     [action.ingredientName] : state.ingredients[action.ingredientName] + 1
-                }
+                },
+                totalPrice : state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
 
             }
         case actionType.REMOVE_INGREDIENTS:
@@ -30,7 +38,9 @@ const reducer = (state = initialState, action) =>{
                 ingredients : {
                     ...state.ingredients,
                     [action.ingredientName] : state.ingredients[action.ingredientName] - 1
-                }
+                },
+                totalPrice : state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
+
 
             }
         default :
